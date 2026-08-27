9c5bd6775f7bbadcaee9f860a858e685176da491
JoeVenner <ylafrimi@gmail.com>
2026-08-27 15:26:00 +0100

    feat(seo): enhance RSS feed with robust feed library and full content support

--- src/app/rss.xml/route.ts
+++ src/app/rss.xml/route.ts
@@ -1,36 +1,50 @@
 import { getAllArticles } from '@/lib/content';
 import { siteConfig } from '@/config/site';
+import { Feed } from 'feed';
 
 export async function GET() {
   const articles = await getAllArticles();
 
-  const feed = `<?xml version="1.0" encoding="utf-8"?>
-  <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom">
-    <channel>
-      <title>${siteConfig.name}</title>
-      <link>${siteConfig.url}</link>
-      <description>${siteConfig.description}</description>
-      <language>en</language>
-      <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
-      <atom:link href="${siteConfig.url}/rss.xml" rel="self" type="application/rss+xml"/>
-      ${articles
-        .map((article) => {
-          return `
-        <item>
-          <title><![CDATA[${article.frontmatter.title}]]></title>
-          <link>${siteConfig.url}/articles/${article.slug}</link>
-          <guid isPermaLink="true">${siteConfig.url}/articles/${article.slug}</guid>
-          <pubDate>${new Date(article.frontmatter.date).toUTCString()}</pubDate>
-          <description><![CDATA[${article.frontmatter.description}]]></description>
-          ${article.frontmatter.category ? `<category>${article.frontmatter.category}</category>` : ''}
-          <author>${article.frontmatter.author || siteConfig.author.name}</author>
-        </item>`;
-        })
-        .join('')}
-    </channel>
-  </rss>`;
+  const feed = new Feed({
+    title: siteConfig.name,
+    description: siteConfig.description,
+    id: siteConfig.url,
+    link: siteConfig.url,
+    language: "en", 
+    image: `${siteConfig.url}${siteConfig.ogImage}`,
+    favicon: `${siteConfig.url}/favicon.ico`,
+    copyright: `All rights reserved ${new Date().getFullYear()}, ${siteConfig.author.name}`,
+    updated: new Date(),
+    feedLinks: {
+      rss2: `${siteConfig.url}/rss.xml`,
+      json: `${siteConfig.url}/feed.json`,
+      atom: `${siteConfig.url}/atom.xml`,
+    },
+    author: {
+      name: siteConfig.author.name,
+      link: siteConfig.links.twitter,
+    }
+  });
 
-  return new Response(feed, {
+  articles.forEach((article) => {
+    feed.addItem({
+      title: article.frontmatter.title,
+      id: `${siteConfig.url}/articles/${article.slug}`,
+      link: `${siteConfig.url}/articles/${article.slug}`,
+      description: article.frontmatter.description,
+      content: article.content, // Includes full content in RSS
+      author: [
+        {
+          name: article.frontmatter.author || siteConfig.author.name,
+        }
+      ],
+      date: new Date(article.frontmatter.date),
+      image: article.frontmatter.image ? `${siteConfig.url}${article.frontmatter.image}` : `${siteConfig.url}/articles/${article.slug}/opengraph-image`,
+      category: article.frontmatter.tags?.map(tag => ({ name: tag })) || []
+    });
+  });
+
+  return new Response(feed.rss2(), {
     headers: {
       'Content-Type': 'application/xml; charset=utf-8',
     },
